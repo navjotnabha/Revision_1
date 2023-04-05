526c96546836f0675ca0f3989f1cf7a87f140667
NAV <navjotnabha@gmail.com>
2023-04-05 21:48:28 +1000

    1) Before Switching To the Action Result <> stuff

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,29 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetVillas()
+        {
+            
+            return Ok(); 
+         
+            return Ok(VillaStore.villaList);
+          
+        }
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)                                            //not public IEnumerable<VillaDTO> GetVilla(int id) because we are returning single villa not list
+        {
+            return VillaStore.villaList.FirstOrDefault(temp => temp.Id == id);
+        }
+
+    }
+}
