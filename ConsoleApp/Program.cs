fa284020c0bfcbbcebbc937555f8ea624fdd633f
NAV <navjotnabha@gmail.com>
2023-04-21 21:20:57 +1000

    Added console app for learning dependency injection

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿class PersonFactory { 
+    public static void Main(string[] args)
+    {
+        string message = "App Executed successfully";
+        ConsoleLogger cl = new ConsoleLogger();
+        cl.Log(message);
+
+        PersonLogger lg=new PersonLogger();
+        lg.Log(message);
+    }     
+}
+
+class PersonLogger : Logger
+{
+    public void Log(string msg)
+    {
+        File.Create("C:\\Users\\NAV\\Desktop\\dams.txt");
+        File.AppendText(msg);
+    }
+}
+class ConsoleLogger : Logger
+{
+    public void Log(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
+public interface Logger
+{
+    public void Log(string msg);
+}
